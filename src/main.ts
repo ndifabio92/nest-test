ad77b384d01f7c22b89f3021017375678163d4e6
Nicolas <nicolasdifabio92@gmail.com>
2025-05-26 00:28:24 -0300

    dto test

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ src/shared/dtos/pagination.dto.spec.ts
@@ -0,0 +1,59 @@
+import 'reflect-metadata';
+import { validate } from 'class-validator';
+import { PaginationDto } from './pagination.dto';
+
+describe('Pagination.dto', () => {
+  it('should validate with default values', async () => {
+    const dto = new PaginationDto();
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(0);
+  });
+
+  it('should validate with valid data', async () => {
+    const dto = new PaginationDto();
+
+    dto.limit = 10;
+    dto.page = 1;
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(0);
+  });
+
+  it('should not validate with invalid page', async () => {
+    const dto = new PaginationDto();
+    dto.page = -1;
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(1);
+
+    errors.forEach((error) => {
+      if (error.property === 'page') {
+        expect(error.constraints?.min).toBeDefined();
+      } else {
+        expect(true).toBeFalsy();
+      }
+    });
+  });
+
+  it('should not validate with invalid limit', async () => {
+    const dto = new PaginationDto();
+
+    dto.limit = -10;
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(1);
+
+    errors.forEach((error) => {
+      if (error.property === 'limit') {
+        expect(error.constraints?.min).toBeDefined();
+      } else {
+        expect(true).toBeFalsy();
+      }
+    });
+  });
+});
